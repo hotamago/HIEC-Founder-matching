47150d1ec148b594add504fe18883d693e84af5d
hotamago <nhson2003@outlook.com>
2024-05-08 12:12:47 +0700

    Fix new list.csv problem

--- module/readData.py
+++ module/readData.py
@@ -7,6 +7,14 @@ class CsvData():
         if not os.path.exists(self.path):
             ValueError('path %s not found' % self.path)
     def read(self, names, valueNan = ""):
+        # check if list.csv is exist
+        if not os.path.exists('list.csv'):
+            # create list.csv by list_example.csv
+            with open('list_example.csv', 'r') as f:
+                with open('list.csv', 'w') as f2:
+                    f2.write(f.read())     
+
+        # read list.csv
         df = pd.read_csv('list.csv', header=0, names=list(names), dtype=str)
         df['id'] = range(0, len(df))
 
